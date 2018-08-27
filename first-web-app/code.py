864ccfe6e00fd370953fa5734540e5cf4fc500a6
cdignam8304 <dignachr@gmail.com>
2018-08-27 18:03:25 -0400

    Add files for first web app

---
+++ first-web-app/code.py
@@ -0,0 +1,43 @@
+# NOTES
+# =====
+# A simple web app.
+# By default, app will run on port 8080, but can change
+# port by running, e.g.:
+# python code.py 1234
+# to run on port 1234.
+
+import web
+
+# tell web.py to look for templates in templates directory:
+render = web.template.render("templates/")
+
+# defines which class relates to which url:
+urls = (
+        # "/(.*)", "index",
+        "/home/(.*)", "index",
+        "/challenge", "challenge"
+        )
+
+# classes:
+
+class index:
+    def GET(self, name):
+        
+        # OPTION 1: Hard-code a variable to be passed to the template:
+        # name = "Chris" # this variable will be passed to html template.
+        # 'index' is the name of the template and 'name' is the variable passed to it.
+        # return render.index(name)
+
+        # OPTION 2: Allow user to input a variable:
+        # user inputs name variable via the url: e.g. /Chris
+        i = web.input(name=None)
+        return render.index(name)
+
+class challenge:
+    def GET(self):
+        return render.challenge()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
